d1a93a9636cff2c434b256de05f0f5766482c949
981393109 <981393109@qq.com>
2020-03-16 19:36:56 +0800

    增加了属性及方法

--- src/main/java/com/zking/rabbitmq/head/aa.java
+++ src/main/java/com/zking/rabbitmq/head/aa.java
@@ -1,4 +1,21 @@
 package com.zking.rabbitmq.head;
 
 public class aa {
+
+        int a  = 123;
+        int b = 456;
+        int c = a+b;
+
+    public int getA() {
+        return a;
+
+    }
+
+    public void setA(int a) {
+        System.out.println("谢沁大傻逼");
+        this.a = a;
+    }
+
+
+
 }
